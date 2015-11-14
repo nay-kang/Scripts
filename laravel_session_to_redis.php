0a16f9ab727d4fc750cdd584cd6d800eb37330ee
nay <kunvunk@gmail.com>
2015-11-15 01:09:01 +0800

    add laravel session to redis script

---
+++ laravel_session_to_redis.php
@@ -0,0 +1,29 @@
+<?php
+define('SESSION_DIR','/data/laravel_session');
+define('REDIS_HOST','127.0.0.1');
+define('REDIS_PORT','6379');
+define('REDIS_AUTH',false);
+define('REDIS_DB',0);
+define('PREFIX','laravel:');
+define('TTL',60*60*24*30);
+
+$d = dir(SESSION_DIR);
+$redis = new Redis();
+$redis->connect(REDIS_HOST,REDIS_PORT);
+if(REDIS_AUTH){
+	$redis->auth(REDIS_AUTH);
+}
+
+$redis->select(0);
+if(REDIS)
+while(false !== ($entry = $d->read())){
+	$file = SESSION_DIR.'/'.$entry;
+	if(is_file($file)){
+		echo $file;
+		$content = file_get_contents($file)
+		$content = serialize(trim($content));
+		$redis->setex(PREFIX.$entry,TTL,$content);
+	}
+}
+
+?>
